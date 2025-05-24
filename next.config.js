6a27558e0dd121a0427ac5b62c2ee1b489cea643
aditya-samal <samaladitya2004@gmail.com>
2025-05-24 12:02:27 +0530

    Caching Removed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply to all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-module.exports = nextConfig;
